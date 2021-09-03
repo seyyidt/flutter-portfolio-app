fe1b7c5bfc4b152ca3e8528e84a6c010232ada2d
seyyid <seyyid.tiryaki@gmail.com>
2021-09-04 00:31:08 +0200

    added footer

--- portfolio_app/lib/view/main_page.dart
+++ portfolio_app/lib/view/main_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:footer/footer.dart';
+import 'package:footer/footer_view.dart';
 import 'package:portfolio_app/view/cv.dart';
 import 'package:portfolio_app/view/skills.dart';
 import 'package:portfolio_app/view/user_top_widget.dart';
@@ -15,32 +17,36 @@ class _MainPageState extends State<MainPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
-      body: Padding(
-        padding: const EdgeInsets.all(32.0),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.only(top: 10, bottom: 10),
-                child: UserTopWidget(),
+      body: new FooterView(
+          footer: new Footer(child: Text("test"), padding: EdgeInsets.all(10)),
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(32.0),
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.only(top: 10, bottom: 10),
+                      child: UserTopWidget(),
+                    ),
+                    Text(
+                      'I am a passionate App Developer located in Vienna.',
+                      textAlign: TextAlign.center,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 15, bottom: 0),
+                      child: SkillsWidget(),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 0, bottom: 5),
+                      child: CVWidget(),
+                    ),
+                  ],
+                ),
               ),
-              Text(
-                'I am a passionate App Developer located in Vienna.',
-                textAlign: TextAlign.center,
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 15, bottom: 0),
-                child: SkillsWidget(),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 0, bottom: 5),
-                child: CVWidget(),
-              ),
-            ],
-          ),
-        ),
-      ),
+            ),
+          ]),
     );
   }
 }
